c82ccedff4eab9f3f43e1082527fc6b0034f0cc3
omi2v <omibedewi@gmail.com>
2022-11-16 01:16:07 -0800

    binary

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,28 @@
+#include "binary_trees.h"
+/**
+ *binary_tree_insert_left - binary tree function
+ *@parent: pparent tree
+ *@value: the value
+ *Return: A pinter node
+ */
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+binary_tree_t *left_node;
+if (parent == NULL)
+return (NULL);
+left_node = malloc(sizeof(binary_tree_t));
+if (left_node == NULL)
+return (NULL);
+left_node->n = value;
+left_node->parent = parent;
+if (parent->left == NULL)
+left_node->left = NULL;
+else
+{
+left_node->left = parent->left;
+parent->left->parent = left_node;
+}
+parent->left = left_node;
+left_node->right = NULL;
+return (left_node);
+}
