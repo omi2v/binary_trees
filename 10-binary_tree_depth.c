95ce6e1aafa9f7daf714eefea065a605f71c457b
omi2v <omibedewi@gmail.com>
2022-11-17 04:16:41 -0800

    binaer10

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -7,7 +7,7 @@
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
 size_t count;
-if (tree == NULL)
+if (tree == NULL || tree->parent == NULL)
 return (0);
 count = binary_tree_depth(tree->parent);
 count++;
